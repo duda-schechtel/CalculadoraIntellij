a7a6dcc21d4884314d08198aa5ef5852e36d7142
Maria Eduarda de andrade Schechtel <duda.schechtel@gmail.com>
2026-05-20 14:24:33 -0300

    refactor: refatora entrada de números.

--- src/main/java/operacoes/Soma.java
+++ src/main/java/operacoes/Soma.java
@@ -1,12 +1,11 @@
 package operacoes;
-
-import java.util.Scanner;
+import imprimirdados.Impressora;
 
 public class Soma {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int x = scanner.nextInt();
-        int y = scanner.nextInt();
-        System.out.println(x + y);
+        Impressora imprimirdados = new Impressora();
+        imprimirdados.lernumeros();
+
+        System.out.println(imprimirdados.x+imprimirdados.y);
     }
 }
